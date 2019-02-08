0d7a674142c9d4ad5eeca2ed87c6b9d6ee291184
Jason Kim <jason@blokable.com>
2019-02-08 12:27:17 -0800

    Negate the bits for clearStrongPullup

--- src/OneWire.cpp
+++ src/OneWire.cpp
@@ -104,7 +104,7 @@ void OneWire::setStrongPullup()
 
 void OneWire::clearStrongPullup()
 {
-	writeConfig(readConfig() & !DS2482_CONFIG_SPU);
+	writeConfig(readConfig() & ~DS2482_CONFIG_SPU);
 }
 
 // Churn until the busy bit in the status register is clear
